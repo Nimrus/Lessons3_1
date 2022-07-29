6c07c7b931a0e683ec58f7a649d9132008df7860
Nimrus <59753998+Nimrus@users.noreply.github.com>
2022-07-29 19:53:31 +0800

    Add files via upload

---
+++ car.js
@@ -0,0 +1,22 @@
+const carName = "volvo";
+let maxCarSpeed = 140;
+let carOwner = "Jayson";
+alert (carName); 
+alert (maxCarSpeed);
+alert (carOwner);
+
+/*
+maxCarSpeed = 110 ;
+console.log ("Скорость машины изменилась");
+maxCarSpeed = 100 ;
+console.log ("Скорость машины уменьшилась");
+maxCarSpeed = 90 ;
+console.log ("Скорость машины упала");
+
+carOwner = "Jim";
+console.log ("Владелей поменялся");
+carOwner = "Nolan";
+console.log ("Владелец другой");
+carOwner = "Frank";
+console.log ("Владелец снова сменился");
+*/
